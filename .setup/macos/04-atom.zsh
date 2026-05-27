faa94cfe4c889d9b8e474b18fe06b2cfe406cad1
Thad Smith <tsmith@employreward.com>
2026-05-27 20:32:52 +0000

    Cleanup settings, add display config, update Brewfile
    - Merge 02-settings.sh into 02-settings.zsh, add kill block, fix shebang
    - Remove 02-settings.sh (was bash-only, ran manually)
    - Remove 04-atom.zsh (Atom is sunset)
    - Add 04-display.zsh: clean dock, hide desktop icons and widgets
    - Brewfile: deprecate old taps, add azure tools, swap insomnia->postman,
      add mssql-tools, trilium-notes, zoom, google-chrome, azure-storage-explorer

--- .setup/macos/04-atom.zsh
+++
@@ -1,4 +0,0 @@
-# install Homebrew
-log::step "Installing Atom Packages"
-
-apm install file-icons go-plus tom-ide-ui ide-go language-protobuf language-vue minimap linter minimap-linter open-no-binaries linter-ui-default go-outline
